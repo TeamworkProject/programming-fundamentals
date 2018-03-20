bababfaa99dc2c815c086a8a20512aa26c738283
KalinKarolev <kkarolev@abv.bg>
2018-03-20 13:11:02 +0200

    Problem 2 Added

---
+++ Problem 2/Problem2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace p02_RectangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double width = double.Parse(Console.ReadLine());
+            double heith = double.Parse(Console.ReadLine());
+
+            double result = width * heith;
+
+            Console.WriteLine($"{result:F2}");
+
+        }
+    }
+}
