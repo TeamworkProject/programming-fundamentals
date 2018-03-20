bbed15f03234780aaf6f2173afb4b9bfaa21186c
KalinKarolev <kkarolev@abv.bg>
2018-03-20 18:44:50 +0200

    Code of Problem 3 is documented

--- intro-and-basic-syntax/solution3.cs
+++ intro-and-basic-syntax/solution3.cs
@@ -2,10 +2,13 @@ using System;
 
 public class Test
 {
-	public static void Main()
-	{
-double a;
-a=double.Parse(Console.ReadLine());
-Console.WriteLine($"{a*1.60934:f2}");
-	}
+    public static void Main()
+    {
+        // We recieve from the Console miles (double). 
+        double a;
+        a = double.Parse(Console.ReadLine());
+
+        //Miles are converted to kilometers and then they are printed. The output is formated to the 2nd decimal place. 
+        Console.WriteLine($"{a * 1.60934:f2}");
+    }
 }
