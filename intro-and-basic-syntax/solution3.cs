9134b256213e5bce1aafdb74b9bf37e11d3e5cf1
KalinKarolev <kkarolev@abv.bg>
2018-03-20 18:57:06 +0200

    Change 'a' in Problem 3 to "miles"

--- intro-and-basic-syntax/solution3.cs
+++ intro-and-basic-syntax/solution3.cs
@@ -5,10 +5,10 @@ public class Test
     public static void Main()
     {
         // We recieve from the Console miles (double). 
-        double a;
-        a = double.Parse(Console.ReadLine());
+        double miles;
+        miles = double.Parse(Console.ReadLine());
 
         //Miles are converted to kilometers and then they are printed. The output is formated to the 2nd decimal place. 
-        Console.WriteLine($"{a * 1.60934:f2}");
+        Console.WriteLine($"{miles * 1.60934:f2}");
     }
 }
