1a45d2318c5cce19fb154d5b277cd67a6266e4c7
Antonio <antonio__1@abv.bg>
2018-03-21 21:56:31 +0200

    Added solution for problem 4

---
+++ Problem 4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Character_Stats
+{
+    public class Class1
+    {
+        public static void Main(string[] args)
+        {
+            var name = Console.ReadLine();
+            var health = int.Parse(Console.ReadLine());
+            var MaxHealth = int.Parse(Console.ReadLine());
+            var energy = int.Parse(Console.ReadLine());
+            var MaxEnergy = int.Parse(Console.ReadLine());
+
+            string currentHealth = "|" + new string('|', health) + new string('.', MaxHealth - health) + "|";
+            string currentEnergy = "|" + new string('|', energy) + new string('.', MaxEnergy - energy) + "|";
+
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Health: {currentHealth}");
+            Console.WriteLine($"Energy: {currentEnergy}");
+        }
+    }
+}
