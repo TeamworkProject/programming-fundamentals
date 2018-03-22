7f0d46594a07b48cdff2b2126bbedeaf2e288316
doctard <jeijei1aa@gmail.com>
2018-03-22 10:59:44 +0200

    commented problem 4

--- Problem 4/Program.cs
+++ Problem 4/Program.cs
@@ -6,15 +6,20 @@ namespace Character_Stats
     {
         public static void Main(string[] args)
         {
+			//character name
             var name = Console.ReadLine();
+			//character current health
             var health = int.Parse(Console.ReadLine());
+			//character max name
             var MaxHealth = int.Parse(Console.ReadLine());
+			//character current energy
             var energy = int.Parse(Console.ReadLine());
+			//character max energy
             var MaxEnergy = int.Parse(Console.ReadLine());
-
+			//current health/energy strings - add '|' equal to health, max health, energy, max energy
             string currentHealth = "|" + new string('|', health) + new string('.', MaxHealth - health) + "|";
             string currentEnergy = "|" + new string('|', energy) + new string('.', MaxEnergy - energy) + "|";
-
+			//print strings
             Console.WriteLine($"Name: {name}");
             Console.WriteLine($"Health: {currentHealth}");
             Console.WriteLine($"Energy: {currentEnergy}");
