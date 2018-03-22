825637d187bf3e8f84273a6a5fd4e00dd55952b5
doctard <jeijei1aa@gmail.com>
2018-03-22 11:19:37 +0200

    changed a/b/c/d in problem 1 to d1/d2/d3/d4

--- Problem 1/problem1.cs
+++ Problem 1/problem1.cs
@@ -5,11 +5,11 @@ public class Test
 {
     public static void Main()
     {
-        int a, b, c, d;
-        a = int.Parse(Console.ReadLine());
-        b = int.Parse(Console.ReadLine());
-        c = int.Parse(Console.ReadLine());
-        d = int.Parse(Console.ReadLine());
-        Console.WriteLine($"{a:d4} {b:d4} {c:d4} {d:d4}");
+        int d1, d2, d3, d4;
+        d1 = int.Parse(Console.ReadLine());
+        d2 = int.Parse(Console.ReadLine());
+        d3 = int.Parse(Console.ReadLine());
+        d4 = int.Parse(Console.ReadLine());
+        Console.WriteLine($"{d1:d4} {d2:d4} {d3:d4} {d4:d4}");
     }
 }
